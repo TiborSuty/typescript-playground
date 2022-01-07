2d59bfca0e0a23926a76ba8ce945f1d8cc7e5f26
Tibor Suty <TiborSuty231@gmail.com>
2022-01-07 21:50:56 +0100

    type-challanges-parameters

--- challenges/easy/parameters.ts
+++ challenges/easy/parameters.ts
@@ -1,5 +1,13 @@
+// Assignment: Implement the built-in Parameters generic without using it.
+
+// Solution
 type CustomParameters<T extends (...args: any[]) => any> = T extends (
   ...args: infer A
 ) => any
   ? A
   : never;
+
+// Test
+type CustomParametersTest = CustomParameters<
+  (a: string, b: number, c: boolean) => string
+>;
--- playground/curry.ts
+++ playground/curry.ts
@@ -139,3 +139,79 @@ const curried10 = curryV5(toCurry09);
 
 const test67 = curried10("Jane", 26)(true, "JJ", "Jini");
 const test68 = curried10("Jane")(26, true, "JJ", "Jini");
+
+type Pos<I extends any[]> = Length<I>;
+type Next<I extends any[]> = Prepend<any, I>;
+type Prev<I extends any[]> = Tail<I>;
+
+type iterator = [any, any];
+type test70 = Pos<iterator>;
+type test71 = Pos<Next<iterator>>;
+type test72 = Pos<Prev<iterator>>;
+
+type CustomIterator<
+  Index extends number = 0,
+  From extends any[] = [],
+  I extends any[] = []
+> = {
+  0: CustomIterator<Index, Next<From>, Next<I>>;
+  1: From;
+}[Pos<I> extends Index ? 1 : 0];
+
+type test73 = CustomIterator<2>;
+type test74 = CustomIterator<3, test73>;
+type test75 = Pos<test73>;
+type test76 = Pos<test74>;
+
+type Reverse<T extends any[], R extends any[] = [], I extends any[] = []> = {
+  0: Reverse<T, Prepend<T[Pos<I>], R>, Next<I>>;
+  1: R;
+}[Pos<I> extends Length<T> ? 1 : 0];
+
+type test77 = Reverse<[1, 2, 3]>;
+type test78 = Reverse<test77>;
+type test79 = Reverse<[2, 1], [3, 4]>;
+
+type Concat<T1 extends any[], T2 extends any[]> = Reverse<
+  Cast<Reverse<T1>, any[]>,
+  T2
+>;
+
+type test80 = Concat<[1, 2], [3, 4]>;
+
+type Append<E, T extends any[]> = Concat<T, [E]>;
+
+type test81 = Append<3, [1, 2]>;
+
+type GapOf<
+  T1 extends any[],
+  T2 extends any[],
+  TN extends any[],
+  I extends any[]
+> = T1[Pos<I>] extends under ? Append<T2[Pos<I>], TN> : TN;
+
+type test82 = GapOf<[under, under], [number, string], [], CustomIterator<0>>;
+type test83 = GapOf<[under, under], [number, string], [], CustomIterator<1>>;
+
+type under = __;
+
+type GapsOf<
+  T1 extends any[],
+  T2 extends any[],
+  TN extends any[] = [],
+  I extends any[] = []
+> = {
+  0: GapsOf<T1, T2, Cast<GapOf<T1, T2, TN, I>, any[]>, Next<I>>;
+  1: Concat<TN, Cast<Drop<Pos<I>, T2>, any[]>>;
+}[Pos<I> extends Length<T1> ? 1 : 0];
+
+type test84 = GapsOf<[under, any, under], [number, string, object, string[]]>
+type test85 = GapsOf<[any, under, any], [number, string, object, string[]]>
+
+type PartialGaps<T extends any[]> = {
+  [K in keyof T]?: T[K] | under
+}
+
+type test87 = PartialGaps<[number, string]>
+
+
