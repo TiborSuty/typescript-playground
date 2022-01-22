535f0d57f7ef51ae44dd393fd9238d809f4600aa
Tibor Suty <TiborSuty231@gmail.com>
2022-01-22 17:59:45 +0100

    lengthOfString

--- challenges/medium/permutation.ts
+++ challenges/medium/permutation.ts
@@ -1,5 +1,6 @@
 // Assignment: Implement permutation type that transforms union types into the array that includes permutations of unions.
 
+// https://github.com/type-challenges/type-challenges/issues/614
 // Solution
 type CustomPermutation<T, U = T> = [T] extends [never]
   ? []
